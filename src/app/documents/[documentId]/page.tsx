d25b5761c76746f64c7897d3074a15f0df202bc1
dafto1 <shreyashnikam1005@gmail.com>
2026-06-28 16:03:25 +0530

    added an state store for editor

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
-import {Editor} from './editor'
+import { Editor } from './editor'
+import {Toolbar} from './toolbar';
 interface DocumentIdPageProps { 
   params: Promise<{ documentId: string }>; 
 }
@@ -6,7 +7,9 @@ const DocumentIdPage =  async ({ params }: DocumentIdPageProps) => {
   const {documentId} = await params
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
-      <h1>{ documentId}</h1>
+      <h1>{documentId}</h1> 
+      <Toolbar>
+      </Toolbar>
       <Editor></Editor>
     </div>
    )
